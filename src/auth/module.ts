f357ae725340dcd86ed2a4f2d4b61db5dff0172e
Anton <anton.merkotun@folderly.com>
2024-10-15 11:12:19 +0200

    first commit

---
+++ src/auth/module.ts
@@ -0,0 +1,45 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './service/auth.service';
+import { AuthController } from './controller/auth.controller';
+import { UserModule } from '../user/module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    UserModule,
+    ConfigModule.forRoot(),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '300s' },
+      }),
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
+
+// import { Module } from '@nestjs/common';
+// import { AuthService } from './service/auth.service';
+// import { AuthController } from './controller/auth.controller';
+// import { UserModule } from '../user/module';
+// import { JwtModule } from '@nestjs/jwt';
+
+// @Module({
+//   imports: [
+//     UserModule,
+//     JwtModule.register({
+//       global: true,
+//       secret: 'jwtSecret',
+//       signOptions: { expiresIn: '300s' },
+//     }),
+//   ],
+//   providers: [AuthService],
+//   controllers: [AuthController],
+// })
+// export class AuthModule {}
---
+++ src/auth/service/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../../user/service/user.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findByUsername(username);
+
+    if (user?.password !== password) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { sub: user.id, username: user.username };
+
+    return {
+      token: await this.jwtService.signAsync(payload),
+    };
+  }
+
+  async signUp(username: string, password: string): Promise<any> {
+    await this.usersService.create(username, password);
+  }
+
+  generateToken(payload: { id: number; username: string }): string {
+    return this.jwtService.sign(payload);
+  }
+}
